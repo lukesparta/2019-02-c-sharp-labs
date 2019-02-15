5ea9df8c239988fb71badd21770dac585e139018
Luke <ldawes@spartaglobal.com>
2019-02-15 08:48:26 +0000

    file names and structure changed. Labs 105 - 107 completed.

---
+++ labs/lab_105_game_name_and_score_01/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using static System.Console;
+
+namespace lab_105_game_name_and_score_01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            if (File.Exists("gamerDetails.txt"))
+            {
+                loadGamer();
+            }
+        }
+
+        //On click of save button
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string[] gamerDet = new string[] { txtGamerName.Text, txtLevel.Text, txtScore.Text };
+
+            //create a file and add gamer details
+            File.WriteAllText("gamerDetails.txt", gamerDet[0] + $"{Environment.NewLine}");
+            File.AppendAllText("gamerDetails.txt", gamerDet[1] + $"{Environment.NewLine}");
+            File.AppendAllText("gamerDetails.txt", gamerDet[2]);
+
+        }
+
+        public void loadGamer()
+        {
+            txtGamerName.Text = File.ReadAllLines("gamerDetails.txt")[0];
+            txtLevel.Text = File.ReadAllLines("gamerDetails.txt")[1];
+            txtScore.Text = File.ReadAllLines("gamerDetails.txt")[2];
+        }
+
+        //Create a Gaming Home Page
+        //Name of gamer (save to text file)
+        //Level reached
+        //Top score
+        //Prize for the best presented interface
+
+
+        //Next iteration  :  add an up/down button to increase the score 106
+
+
+
+        //List box - List all folders in project root 107
+        //Manually create folders
+
+
+
+        //List box - List all folders in project root and list files in seperate list box 108
+        //Manually create folders and files
+
+
+
+        //Add a textbox for a new file name 109
+        //Add a textblock for data to add to file
+        //Add a save button to create a new text file
+        //Permit Exception if file exists
+    }
+}
--- labs/random_person/Program.cs
+++ labs/random_person/Program.cs
@@ -10,6 +10,40 @@ namespace random_person
     {
         static void Main(string[] args)
         {
+            
         }
+    }
+
+    class Parent
+    {
+        //properties
+        public string name;
+        public int age;
+        public DateTime dob;
+        
+        //array of 10 people
+        string[] people = new string[] { "Luke Dawes", "John Doe",
+                                        "James Carter", "Travis Scott",
+                                        "Donald Trump", "Burno Mars",
+                                        "Justin Beiber", "Frank Ocean",
+                                        "Beyonce Knowles", "Lyrn Hill"};
+
+        //randomly select person from array
+        public int randomNumberGen()
+        {
+            Random randomNum = new Random();
+            int rn = randomNum.Next(people.Length);
+            return rn;
+        }
+
+        
+        /*
+         Use a randomise function to pick a random name from the array, also an age 1 to 100, 
+       also a date constructed from dd/mm/yyyy where dd,mm and yyyy are randomly generated
+     Using our random data instantiate a new instance of our parent class to create a new object
+     Add this new instance to a list of people  myList.add(person01)
+     Repeat every second for 100 seconds  system.threading.thread.sleep(1000)
+     Put this onto a WPF app and push to a label, with the new person appearing every second
+         */
     }
 }
